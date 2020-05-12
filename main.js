63fd6b92d958102027ff995f2a4988817882a91e
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2020-05-12 20:48:02 +0200

    Modified DOM using JS

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ const surname = "Bartkowicz";
 const age = 29;
 
 console.log(`Mam na imę ${name + " " + surname} i mam ${age} lat.`);
+
+const empty = document.querySelector('.innerHTML');
+empty.innerHTML = 'Uźycie innerHTML na potrzeby modyfikacji DOM, praktyczne uźycie JS modyfikujązego DOM';
+empty.style.color = 'blue';
